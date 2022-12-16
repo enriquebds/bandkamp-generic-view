4fdcee124e479a14a4af95b75d3fa33bd64238b4
Enrique <enrique.barbosasilva@gmail.com>
2022-12-16 08:01:58 -0300

    refactor: code-changed

--- albums/serializers.py
+++ albums/serializers.py
@@ -16,4 +16,5 @@ from .models import Album
 class AlbumSerializer(serializers.ModelSerializer):
     class Meta:
         model = Album
-        fields = "__all__"
+        fields = ["id", "name", "year", "user_id"]
+        read_only_fields = ["user_id"]
