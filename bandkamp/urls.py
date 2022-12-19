8920c9578c308225c57a7e27455ae0ddd6269d69
Enrique <enrique.barbosasilva@gmail.com>
2022-12-19 18:56:38 -0300

    refactor: code-changed

--- bandkamp/urls.py
+++ bandkamp/urls.py
@@ -15,10 +15,26 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("users.urls")),
     path("api/", include("albums.urls")),
     # path("api/", include("songs.urls")),
+    path("docs/", SpectacularAPIView.as_view(), name="docs"),
+    path(
+        "docs/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="docs"),
+        name="swagger-ui",
+    ),
+    path(
+        "docs/redoc/",
+        SpectacularRedocView.as_view(url_name="docs"),
+        name="redoc",
+    ),
 ]
